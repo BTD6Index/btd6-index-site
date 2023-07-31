0ce37ee2faf601a9e791829a03685a1678552fb6
Landmaster <python0429@gmail.com>
2023-07-30 21:04:45 -0400

    response json isn't a constructor

--- functions/fetch-2tc.js
+++ functions/fetch-2tc.js
@@ -2,5 +2,5 @@ export function onRequest(context) {
     let query_result = context.env.BTD6_INDEX_DB
     .prepare("SELECT tower1,tower2,person,link FROM 2tccompletions WHERE person=?")
     .bind('KaptainKO').all();
-    return new Response.json(query_result); // TODO update this to be real shit
+    return Response.json(query_result); // TODO update this to be real shit
 }
