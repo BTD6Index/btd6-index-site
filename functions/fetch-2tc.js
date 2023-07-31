a01cfb02e0dfbc88bfe9ece08d71d66c9437ce65
Landmaster <python0429@gmail.com>
2023-07-30 21:18:36 -0400

    add async

--- functions/fetch-2tc.js
+++ functions/fetch-2tc.js
@@ -1,5 +1,5 @@
-export function onRequest(context) {
-    let query_result = context.env.BTD6_INDEX_DB
+export async function onRequest(context) {
+    let query_result = await context.env.BTD6_INDEX_DB
     .prepare("SELECT tower1,tower2,person,link FROM '2tccompletions' WHERE person=?1")
     .bind('RB').all();
     return Response.json(query_result); // TODO update this to be real shit
