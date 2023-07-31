ee0b9a5a899ad0b66fa2af3781a09f336e61f8f9
Landmaster <python0429@gmail.com>
2023-07-30 20:52:59 -0400

    query cloudflare db

--- functions/fetch-2tc.js
+++ functions/fetch-2tc.js
@@ -1,10 +1,6 @@
 export function onRequest(context) {
-    return new Response(JSON.stringify([
-        {
-            tower1: "Energizer",
-            tower2: "The Anti-Bloon",
-            person: "KyrosQF",
-            link: "https://youtu.be/dlvYT_VZBOI"
-        }
-    ])); // TODO update this to be real shit
+    let query_result = context.env.BTD6_INDEX_DB
+    .prepare("SELECT tower1,tower2,person,link FROM 2tccompletions WHERE person=?")
+    .bind('KaptainKO').all();
+    return new Response.json(query_result); // TODO update this to be real shit
 }
