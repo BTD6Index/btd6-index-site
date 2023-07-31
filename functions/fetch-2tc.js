dec0dfe1cc93cd45165add3495bce45146c5780e
Landmaster <python0429@gmail.com>
2023-07-30 21:24:47 -0400

    use results key of api results for d1

--- functions/fetch-2tc.js
+++ functions/fetch-2tc.js
@@ -2,5 +2,5 @@ export async function onRequest(context) {
     let query_result = await context.env.BTD6_INDEX_DB
     .prepare("SELECT tower1,tower2,person,link FROM '2tccompletions' WHERE person=?1")
     .bind('RB').all();
-    return Response.json(query_result); // TODO update this to be real shit
+    return Response.json(query_result['results']); // TODO update this to be real shit
 }
