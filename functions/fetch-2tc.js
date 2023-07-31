660a59829b8d7a6c56b5a9a156b80d380ab3c668
Landmaster <python0429@gmail.com>
2023-07-30 21:08:43 -0400

    fix some shit in query

--- functions/fetch-2tc.js
+++ functions/fetch-2tc.js
@@ -1,6 +1,6 @@
 export function onRequest(context) {
     let query_result = context.env.BTD6_INDEX_DB
-    .prepare("SELECT tower1,tower2,person,link FROM 2tccompletions WHERE person=?")
+    .prepare("SELECT tower1,tower2,person,link FROM '2tccompletions' WHERE person=?")
     .bind('KaptainKO').all();
-    return Response.json(query_result); // TODO update this to be real shit
+    return Response.json(query_result.results); // TODO update this to be real shit
 }
