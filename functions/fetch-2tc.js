88962f2eb726d8c92823ef8aa61f6c9b8310cb40
Landmaster <python0429@gmail.com>
2023-07-31 21:49:03 -0400

    add ability to go to next and prev page of 2tc results

--- functions/fetch-2tc.js
+++ functions/fetch-2tc.js
@@ -1,14 +1,16 @@
 export async function onRequest(context) {
     let search_params = new URL(context.request.url).searchParams;
     let query = search_params.get('query');
+    let offset = search_params.get('offset') ?? '0';
     let query_stmt;
     if (query) {
         query_stmt = context.env.BTD6_INDEX_DB
-        .prepare('SELECT * FROM "2tc_completions" WHERE "2tc_completions" = ?1 ORDER BY tower1, tower2, map LIMIT 10')
-        .bind(query);
+        .prepare(`SELECT * FROM "2tc_completions" WHERE "2tc_completions" = ?1 ORDER BY tower1, tower2, map LIMIT 10 OFFSET ?2`)
+        .bind(query, offset);
     } else {
         query_stmt = context.env.BTD6_INDEX_DB
-        .prepare('SELECT * FROM "2tc_completions" ORDER BY tower1, tower2, map LIMIT 10');
+        .prepare(`SELECT * FROM "2tc_completions" ORDER BY tower1, tower2, map LIMIT 10 OFFSET ?1`)
+        .bind(offset);
     }
     let query_result = await query_stmt.all();
     return Response.json(query_result['results']);
--- src/routes/challenges/TwoTC.js
+++ src/routes/challenges/TwoTC.js
@@ -2,40 +2,60 @@ import { useEffect, useState } from "react"
 
 export default function TwoTC() {
     const [query, setQuery] = useState('');
+    const [offset, setOffset] = useState(0);
     const [completions, setCompletions] = useState([]);
 
     useEffect(() => {
         fetch("/fetch-2tc?" + new URLSearchParams({
-            query: query
+            query: query,
+            offset: offset
         }))
         .then(response => response.json())
         .then(data => {
             setCompletions(data);
         })
         .catch(err => console.log(err));
-    }, [query]);
+    }, [query, offset]);
+
+    const onSearch = (e) => {
+        setOffset(0);
+        setQuery(e.target.value);
+    };
+
+    const onPrev = () => {
+        setOffset(offset - 10);
+    }
+
+    const onNext = () => {
+        setOffset(offset + 10);
+    };
 
     return <>
         <h1>Two Towers CHIMPS</h1>
-        <input type="text" name="search" id="searchbar" placeholder="Search" value={query} onChange={e => setQuery(e.target.value)} />
-        <input type="submit" name="submit" value="Search" />
+        <input type="text" name="search" id="searchbar" placeholder="Search" value={query} onChange={onSearch} />
         <table>
-            <tr>
-                <th>Tower #1</th>
-                <th>Tower #2</th>
-                <th>Player</th>
-                <th>Completion Link</th>
-            </tr>
-            {
-                completions.map(
-                    completion => <tr>
-                        <td>{completion.tower1}</td>
-                        <td>{completion.tower2}</td>
-                        <td>{completion.person}</td>
-                        <td><a href={completion.link}>Link</a></td>
-                    </tr>
-                )
-            }
+            <thead>
+                <tr>
+                    <th>Tower #1</th>
+                    <th>Tower #2</th>
+                    <th>Player</th>
+                    <th>Completion Link</th>
+                </tr>
+            </thead>
+            <tbody>
+                {
+                    completions.map(
+                        completion => <tr>
+                            <td>{completion.tower1}</td>
+                            <td>{completion.tower2}</td>
+                            <td>{completion.person}</td>
+                            <td><a href={completion.link}>Link</a></td>
+                        </tr>
+                    )
+                }
+            </tbody>
         </table>
+        <button type="button" onClick={onPrev}>Previous</button>
+        <button type="button" onClick={onNext}>Next</button>
     </>
 };
