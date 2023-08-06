d743b3e48d5ae2a05af37d00add7db5c72f32ebe
Landmaster <python0429@gmail.com>
2023-08-05 21:19:33 -0400

    auto-embed latest yt vid

--- src/routes/HomePage.js
+++ src/routes/HomePage.js
@@ -3,7 +3,7 @@ function HomePage() {
         <h1>High-Level Bloons TD 6 Challenges</h1>
         <p>We are a community of Bloons players tracking various community-created challenges.</p>
         <h2>Latest Video</h2>
-        <iframe id="latestvid" src="https://www.youtube-nocookie.com/embed/YfmKsXnD8dY" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+        <iframe id="latestvid" src="https://www.youtube-nocookie.com/embed?listType=playlist&list=UULFFbnVzrtC3yShaAqwbAsyPw" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
         <h2>Join Us</h2>
         <p><a href="https://www.youtube.com/@btd6index">Youtube</a> | <a href="https://discord.gg/RAGfmAB">Discord</a></p>
     </>;
