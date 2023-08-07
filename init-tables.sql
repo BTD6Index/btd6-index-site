8c1ce996ecf6b60f5b035c701c668486bf5482e1
Landmaster <python0429@gmail.com>
2023-08-07 08:58:47 -0400

    add table initializer sql script

---
+++ init-tables.sql
@@ -0,0 +1,4 @@
+CREATE VIRTUAL TABLE '2tc_completions' USING fts5(tower1, tower2, map, person, link, og);
+CREATE VIRTUAL TABLE '2mp_completions' USING fts5(entity, map, person, link, og);
+CREATE TABLE '2tc_extra_info' (tower1, tower2, upgrade1, upgrade2, version, date);
+CREATE TABLE '2mp_extra_info' (entity, upgrade, version, date);
