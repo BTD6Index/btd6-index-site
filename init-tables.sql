872bc830367486ffeb9e63114e447b19d18f4d87
Landmaster <python0429@gmail.com>
2023-08-15 22:13:22 -0400

    add space after begin keyword in sql

--- init-tables.sql
+++ init-tables.sql
@@ -5,20 +5,16 @@ CREATE TABLE '2tc_completions' (
 CREATE VIRTUAL TABLE '2tc_completions_fts' USING fts5(
     tower1, tower2, map, person, link, og, pending UNINDEXED, content='2tc_completions', tokenize='unicode61 remove_diacritics 2', prefix='1 2 3'
 );
-CREATE TRIGGER '2tc_completions_ai' AFTER INSERT ON '2tc_completions' BEGIN
-    INSERT INTO '2tc_completions_fts' (rowid, tower1, tower2, map, person, link, og, pending)
-    VALUES (new.rowid, new.tower1, new.tower2, new.map, new.person, new.link, new.og, new.pending);
+CREATE TRIGGER '2tc_completions_ai' AFTER INSERT ON '2tc_completions' BEGIN 
+    INSERT INTO '2tc_completions_fts' (rowid, tower1, tower2, map, person, link, og, pending) VALUES (new.rowid, new.tower1, new.tower2, new.map, new.person, new.link, new.og, new.pending);
 END;
-CREATE TRIGGER '2tc_completions_ad' AFTER DELETE ON '2tc_completions' BEGIN
-    INSERT INTO '2tc_completions_fts' ('2tc_completions_fts', rowid, tower1, tower2, map, person, link, og, pending)
-    VALUES ('delete', old.rowid, old.tower1, old.tower2, old.map, old.person, old.link, old.og, old.pending);
+CREATE TRIGGER '2tc_completions_ad' AFTER DELETE ON '2tc_completions' BEGIN 
+    INSERT INTO '2tc_completions_fts' ('2tc_completions_fts', rowid, tower1, tower2, map, person, link, og, pending) VALUES ('delete', old.rowid, old.tower1, old.tower2, old.map, old.person, old.link, old.og, old.pending);
 END;
-CREATE TRIGGER '2tc_completions_au' AFTER UPDATE ON '2tc_completions' BEGIN
-    INSERT INTO '2tc_completions_fts' ('2tc_completions_fts', rowid, tower1, tower2, map, person, link, og, pending)
-    VALUES ('delete', old.rowid, old.tower1, old.tower2, old.map, old.person, old.link, old.og, old.pending);
+CREATE TRIGGER '2tc_completions_au' AFTER UPDATE ON '2tc_completions' BEGIN 
+    INSERT INTO '2tc_completions_fts' ('2tc_completions_fts', rowid, tower1, tower2, map, person, link, og, pending) VALUES ('delete', old.rowid, old.tower1, old.tower2, old.map, old.person, old.link, old.og, old.pending);
 
-    INSERT INTO '2tc_completions_fts' (rowid, tower1, tower2, map, person, link, og, pending)
-    VALUES (new.rowid, new.tower1, new.tower2, new.map, new.person, new.link, new.og, new.pending);
+    INSERT INTO '2tc_completions_fts' (rowid, tower1, tower2, map, person, link, og, pending) VALUES (new.rowid, new.tower1, new.tower2, new.map, new.person, new.link, new.og, new.pending);
 END;
 CREATE TABLE '2tc_extra_info' (
     tower1, tower2, upgrade1, upgrade2, version, date, PRIMARY KEY (tower1, tower2)
@@ -30,20 +26,16 @@ CREATE TABLE '2mp_completions' (
 CREATE VIRTUAL TABLE '2mp_completions_fts' USING fts5(
     entity, map, person, link, og, pending UNINDEXED, content='2mp_completions', tokenize='unicode61 remove_diacritics 2', prefix='1 2 3'
 );
-CREATE TRIGGER '2mp_completions_ai' AFTER INSERT ON '2mp_completions' BEGIN
-    INSERT INTO '2mp_completions_fts' (rowid, entity, map, person, link, og, pending)
-    VALUES (new.rowid, new.entity, new.map, new.person, new.link, new.og, new.pending);
+CREATE TRIGGER '2mp_completions_ai' AFTER INSERT ON '2mp_completions' BEGIN 
+    INSERT INTO '2mp_completions_fts' (rowid, entity, map, person, link, og, pending) VALUES (new.rowid, new.entity, new.map, new.person, new.link, new.og, new.pending);
 END;
-CREATE TRIGGER '2mp_completions_ad' AFTER DELETE ON '2mp_completions' BEGIN
-    INSERT INTO '2mp_completions_fts' ('2mp_completions_fts', rowid, entity, map, person, link, og, pending)
-    VALUES ('delete', old.rowid, old.entity, old.map, old.person, old.link, old.og, old.pending);
+CREATE TRIGGER '2mp_completions_ad' AFTER DELETE ON '2mp_completions' BEGIN 
+    INSERT INTO '2mp_completions_fts' ('2mp_completions_fts', rowid, entity, map, person, link, og, pending) VALUES ('delete', old.rowid, old.entity, old.map, old.person, old.link, old.og, old.pending);
 END;
-CREATE TRIGGER '2mp_completions_au' AFTER UPDATE ON '2mp_completions' BEGIN
-    INSERT INTO '2mp_completions_fts' ('2mp_completions_fts', rowid, entity, map, person, link, og, pending)
-    VALUES ('delete', old.rowid, old.entity, old.map, old.person, old.link, old.og, old.pending);
+CREATE TRIGGER '2mp_completions_au' AFTER UPDATE ON '2mp_completions' BEGIN 
+    INSERT INTO '2mp_completions_fts' ('2mp_completions_fts', rowid, entity, map, person, link, og, pending) VALUES ('delete', old.rowid, old.entity, old.map, old.person, old.link, old.og, old.pending);
 
-    INSERT INTO '2mp_completions_fts' (rowid, entity, map, person, link, og, pending)
-    VALUES (new.rowid, new.entity, new.map, new.person, new.link, new.og, new.pending);
+    INSERT INTO '2mp_completions_fts' (rowid, entity, map, person, link, og, pending) VALUES (new.rowid, new.entity, new.map, new.person, new.link, new.og, new.pending);
 END;
 CREATE TABLE '2mp_extra_info' (
     entity PRIMARY KEY, upgrade, version, date
