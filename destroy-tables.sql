5c72aba56ea342ac1fe602a368950f60f5b965b7
Landmaster <python0429@gmail.com>
2023-08-16 18:45:21 -0400

    update destroy tables sql

--- destroy-tables.sql
+++ destroy-tables.sql
@@ -4,6 +4,7 @@ DROP TRIGGER IF EXISTS '2tc_completions_ai';
 DROP TRIGGER IF EXISTS '2tc_completions_ad';
 DROP TRIGGER IF EXISTS '2tc_completions_au';
 DROP TABLE IF EXISTS '2tc_extra_info';
+DROP TABLE IF EXISTS '2tc_completion_notes';
 
 DROP TABLE IF EXISTS '2mp_completions';
 DROP TABLE IF EXISTS '2mp_completions_fts';
@@ -11,6 +12,7 @@ DROP TRIGGER IF EXISTS '2mp_completions_ai';
 DROP TRIGGER IF EXISTS '2mp_completions_ad';
 DROP TRIGGER IF EXISTS '2mp_completions_au';
 DROP TABLE IF EXISTS '2mp_extra_info';
+DROP TABLE IF EXISTS '2mp_completion_notes';
 
 DROP TABLE IF EXISTS 'fttc_completions';
 DROP TABLE IF EXISTS 'ltc_completions';
