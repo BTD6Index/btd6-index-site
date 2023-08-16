4e26776084aa2a084d3d49eec1e0c4016bb68f55
Landmaster <python0429@gmail.com>
2023-08-15 22:25:15 -0400

    simplify add/edit challenge endpoints

--- functions/member/handleAddSubmit.js
+++ functions/member/handleAddSubmit.js
@@ -54,26 +54,19 @@ async function handleAddSubmit({context, challenge, fields, extraInfoFields}) {
     }
 
     const shared_fields = fields.filter(field => extraInfoFields.includes(field));
-
-    const in_place_update = edit_mode && fields.every(field => form_data.get(`edited-${field}`) === form_data.get(field));
-
-    const completion_already_exists = `SELECT * FROM "${challenge}_completions" WHERE ${in_place_update ? 'FALSE' : 'TRUE'} AND ${fieldsCondition(fields, 1)}`;
-
+    
     let add_completion_stmt;
     let add_info_stmt;
     if (edit_mode) {
         add_completion_stmt = `UPDATE "${challenge}_completions" SET (${fields.join(',')},person,link,og,pending) = (${paramsList(0, fields.length+4)}) `
-        + `WHERE ${fieldsCondition(fields, fields.length + 5)} AND ${is_helper ? 'TRUE' : `pending = ?${fields.length + 4}`} `
-        + `AND NOT EXISTS (${completion_already_exists})`;
+        + `WHERE ${fieldsCondition(fields, fields.length + 5)} AND ${is_helper ? 'TRUE' : `pending = ?${fields.length + 4}`} RETURNING *`;
         add_info_stmt = `UPDATE "${challenge}_extra_info" SET (${extraInfoFields.join(',')}) = (${paramsList(0, extraInfoFields.length)}) WHERE ${fieldsCondition(shared_fields, extraInfoFields.length + 1)}`;
     } else {
-        add_completion_stmt = `INSERT INTO "${challenge}_completions" (${fields.join(',')},person,link,og,pending) SELECT ${paramsList(0, fields.length+4)} `
-        + `WHERE NOT EXISTS (${completion_already_exists}) RETURNING *`;
-        add_info_stmt = `INSERT INTO "${challenge}_extra_info" VALUES (${paramsList(0, extraInfoFields.length)}) RETURNING *`;
+        add_completion_stmt = `INSERT INTO "${challenge}_completions" (${fields.join(',')},person,link,og,pending) VALUES (${paramsList(0, fields.length+4)}) RETURNING *`;
+        add_info_stmt = `INSERT INTO "${challenge}_extra_info" VALUES (${paramsList(0, extraInfoFields.length)})`;
     }
     
     let batch = [
-        db.prepare(completion_already_exists).bind(...fields.map(field => form_data.get(field))),
         db.prepare(add_completion_stmt)
         .bind(
             ...fields.map(field => form_data.get(field)),
@@ -104,7 +97,7 @@ async function handleAddSubmit({context, challenge, fields, extraInfoFields}) {
         return respondError(e.message);
     }
     
-    let inserted = batch_result[0].results.length == 0;
+    let inserted = batch_result[0].results.length > 0;
 
     if (inserted && hasImage) {
         // upload only when uuid doesn't exist
