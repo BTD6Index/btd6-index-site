d0fd5a7013eb636abccfc548e2713a7a2438de82
Landmaster <python0429@gmail.com>
2023-08-19 17:42:35 -0400

    add note that sholda/shol gerald are banned from 2tc

--- src/routes/challenges/TwoTC.js
+++ src/routes/challenges/TwoTC.js
@@ -4,7 +4,7 @@ export default function TwoMPC() {
     return <ChallengePage
     challenge="2tc"
     header="2 Towers CHIMPS"
-    description="In this challenge, win a game of CHIMPS with buying exactly two towers (including heroes)."
+    description="In this challenge, win a game of CHIMPS with buying exactly two towers (including heroes and excluding towers/heroes that have, in the past or currently, solo CHIMPS as a single tower)."
     fieldHeaders={['Tower 1', 'Tower 2']}
     fields={['tower1', 'tower2']}
     />
--- src/routes/challenges/rules/TwoTCRules.js
+++ src/routes/challenges/rules/TwoTCRules.js
@@ -3,9 +3,10 @@ export default function TwoTCRules() {
         <p><a href="/2tc">Back to 2TCs</a></p>
         <h1>Rules for 2TC</h1>
         <ol>
+            <li>Any tower that (in the past or currently) soloes CHIMPS entirely on its own is disallowed. At the time of writing, they are <strong>Sauda</strong> and <strong>Geraldo</strong>.</li>
             <li>Challenge editor should be set to hard difficulty and CHIMPS mode.
 Default values for Starting Cash, Staring Lives, Max Lives, Start Round, and End Round are acceptable.
-Under no circumstances should your end round be set to r101, r120, r140, or r300.</li>
+<strong>Under no circumstances should your end round be set to r101, r120, r140, or r300.</strong></li>
             <li>Least Cash condition set to "No Score Set" is required for any run being submitted (you can easily reset this after winning a run by clicking auto twice)</li>
             <li>Victory Screen submissions are not sufficient enough to go on The Index, please provide a screenshot of the final setup.</li>
             <li>A challenge code (restricted to the 2 towers used) is required.</li>
