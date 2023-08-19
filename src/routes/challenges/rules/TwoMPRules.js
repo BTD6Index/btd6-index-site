1df67a91f7d5b6967c6f051d783c99dc9e5fd89d
Landmaster <python0429@gmail.com>
2023-08-19 17:45:06 -0400

    formatting

--- src/routes/challenges/rules/TwoMPRules.js
+++ src/routes/challenges/rules/TwoMPRules.js
@@ -3,9 +3,11 @@ export default function TwoMPRules() {
         <p><a href="/2mp">Back to 2MPs</a></p>
         <h1>Rules for 2MP</h1>
         <ol>
-            <li>Challenge editor should be set to hard difficulty and CHIMPS mode.
-Default values for Starting Cash, Staring Lives, Max Lives, Start Round, and End Round are acceptable.
-<strong>Under no circumstances should your end round be set to r101, r120, r140, or r300.</strong></li>
+            <li>
+                Challenge editor should be set to hard difficulty and CHIMPS mode.
+                Default values for Starting Cash, Staring Lives, Max Lives, Start Round, and End Round are acceptable.
+                <strong>Under no circumstances should your end round be set to r101, r120, r140, or r300.</strong>
+            </li>
             <li>Least Cash condition set to "No Score Set" is required for any run being submitted (you can easily reset this after winning a run by clicking auto twice)</li>
             <li>
                 Please provide a screenshot of both the final setup and the victory screen (alternatively, both can be in a recording).
