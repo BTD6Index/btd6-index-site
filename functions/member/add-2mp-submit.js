2bf8b3f30fd78dbff4f0d3ea231c0dcffb0cb31a
Landmaster <python0429@gmail.com>
2023-08-19 15:47:33 -0400

    discord webhooks should properly display images now

--- functions/member/add-2mp-submit.js
+++ functions/member/add-2mp-submit.js
@@ -6,17 +6,9 @@ export async function onRequest(context) {
         fields: ['entity', 'map'],
         extraInfoFields: ['entity', 'upgrade', 'version', 'date'],
         genEmbedFunction: ({link, formData, edit, filekey}) => ({
-            "content": `${link || `https://media.btd6index.win/${filekey}`}`,
-            "embeds": [
-                {
-                    "title": `${formData.get('entity')} 2MPC on ${formData.get('map')} ${
-                        edit ? 'Edited' : 'Submitted'
-                    }${formData.has('verify') ? ' and Verified' : ''}`,
-                    "description": `Person: ${formData.get('person')}\nView 2MPs: https://btd6index.win/2mp`,
-                    "color": 0x2d00c1,
-                    "url": link || `https://media.btd6index.win/${filekey}`
-                }
-            ],
+            "content": `**${formData.get('entity')} 2MPC on ${formData.get('map')} ${
+                edit ? 'Edited' : 'Submitted'
+            }${formData.has('verify') ? ' and Verified' : ''}**\nPerson: ${formData.get('person')}\nLink: ${link || `https://media.btd6index.win/${filekey}`}\nView 2MPs: https://btd6index.win/2mp`,
             "username": "Glue Rat",
             "avatar_url": "https://btd6index.win/static/media/GlueGunnerPetRatIcon.949fcb9e188713ce4e4e.png",
             "attachments": []
--- functions/member/add-2tc-submit.js
+++ functions/member/add-2tc-submit.js
@@ -6,17 +6,9 @@ export async function onRequest(context) {
         fields: ['tower1', 'tower2', 'map'],
         extraInfoFields: ['tower1', 'tower2', 'upgrade1', 'upgrade2', 'version', 'date'],
         genEmbedFunction: ({ link, formData, edit, filekey }) => ({
-            "content": `${link || `https://media.btd6index.win/${filekey}`}`,
-            "embeds": [
-                {
-                    "title": `${formData.get('tower1')} and ${formData.get('tower2')} 2TC on ${formData.get('map')} ${
-                        edit ? 'Edited' : 'Submitted'
-                    }${formData.has('verify') ? ' and Verified' : ''}`,
-                    "description": `Person: ${formData.get('person')}\nView 2TCs: https://btd6index.win/2tc`,
-                    "color": 0xc40707,
-                    "url": link || `https://media.btd6index.win/${filekey}`
-                }
-            ],
+            "content": `**${formData.get('tower1')} and ${formData.get('tower2')} 2TC on ${formData.get('map')} ${
+                edit ? 'Edited' : 'Submitted'
+            }${formData.has('verify') ? ' and Verified' : ''}**\nPerson: ${formData.get('person')}\nLink: ${link || `https://media.btd6index.win/${filekey}`}\nView 2TCs: https://btd6index.win/2tc`,
             "username": "Glue Rat",
             "avatar_url": "https://btd6index.win/static/media/GlueGunnerPetRatIcon.949fcb9e188713ce4e4e.png",
             "attachments": []
