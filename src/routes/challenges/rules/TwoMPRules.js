3e511afa372aeacf0bf66c9d1a3b79004c403afd
Landmaster <python0429@gmail.com>
2023-08-20 15:37:40 -0400

    update 2tc/2mp rules

--- src/routes/challenges/rules/TwoMPRules.js
+++ src/routes/challenges/rules/TwoMPRules.js
@@ -2,19 +2,27 @@ export default function TwoMPRules() {
     return <>
         <p><a href="/2mp">Back to 2MPs</a></p>
         <h1>Rules for 2MP</h1>
-        <ol>
-            <li>
-                Challenge editor should be set to hard difficulty and CHIMPS mode.
-                Default values for Starting Cash, Starting Lives, Max Lives, Start Round, and End Round should be kept.
-                Sliders (regrow rate, etc.) should be at their default value.
-                <strong> Under no circumstances should your end round be set to r101, r120, r140, or r300.</strong>
-            </li>
-            <li>Least Cash condition set to "No Score Set" is required for any run being submitted (you can easily reset this after winning a run by clicking auto twice)</li>
-            <li>
-                Please provide a screenshot of both the final setup and the victory screen (alternatively, both can be in a recording).
-            </li>
-            <li>Any Adora run, as well as any 2MP run on Geared/both Puddles maps/Ouch/Quad/Dark Dungeons/Ravine/Erosion/X Factor, needs full recordings.</li>
-            <li>Any hard rounds need to be recorded.</li>
-        </ol>
+        <h2>Challenge Settings</h2>
+        <p>
+            The Challenge Editor should be set to the hard difficulty and CHIMPS mode.
+            Starting Cash, Starting Lives, Max Lives, Start Round, and End Round must be set to their default values.
+            The end round must strictly be set to Round 100.
+            The "Least Cash" condition should be set to "No Score Set" (or 99999999 through clicking the auto button twice which does the same thing without needing a reset); this is mainly to prevent exploiting of cash bugs. You can reset the score after completing a run by clicking the auto button twice.
+            All sliders must be set to 100%.
+            All check boxes at the bottom must remain unchecked.
+        </p>
+        <h2>Submission Requirements</h2>
+        <p>
+            Victory Screen screenshots alone are insufficient for inclusion in The Index. You must also provide a screenshot of the final tower setup (with the 2MP'ed tower selected unless something like the Covered Garden covers prevents this).
+        </p>
+        <h2>Hard Round Recordings</h2>
+        <p>
+            For the successful completion of a submission, recordings of gameplay during hard rounds are mandatory. If a helper from the community requests recordings of a particular round, you will have 48 hours to provide the requested footage. Failure to do so within this timeframe may result in a temporary suspension or ban from future submissions.
+            Additionally, any runs featuring Adora, as well as any runs on Geared, either of the Puddles maps, #Ouch, Quad, Ravine, Dark Dungeons, X Factor, or Erosion, must include full recordings of gameplay.
+        </p>
+        <h2>Fair Play and Game Integrity</h2>
+        <p>
+            No game modification, hacking, or unauthorized software use allowed. This includes the use of speedhacks and any other alterations that deviate from the game's original mechanics and settings. Violation leads to disqualification and potential exclusion from future challenges.
+        </p>
     </>;
 }
--- src/routes/challenges/rules/TwoTCRules.js
+++ src/routes/challenges/rules/TwoTCRules.js
@@ -2,19 +2,36 @@ export default function TwoTCRules() {
     return <>
         <p><a href="/2tc">Back to 2TCs</a></p>
         <h1>Rules for 2TC</h1>
-        <ol>
-            <li>Any tower that (in the past or currently) soloes CHIMPS entirely on its own is disallowed. At the time of writing, they are <strong>Sauda</strong> and <strong>Geraldo</strong>.</li>
-            <li>
-                Challenge editor should be set to hard difficulty and CHIMPS mode.
-                Default values for Starting Cash, Starting Lives, Max Lives, Start Round, and End Round should be kept.
-                Sliders (regrow rate, etc.) should be at their default value.
-                <strong> Under no circumstances should your end round be set to r101, r120, r140, or r300.</strong>
-            </li>
-            <li>Least Cash condition set to "No Score Set" is required for any run being submitted (you can easily reset this after winning a run by clicking auto twice)</li>
-            <li>Victory Screen submissions are not sufficient enough to go on The Index, please provide a screenshot of the final setup.</li>
-            <li>A challenge code (restricted to the 2 towers used) is required.</li>
-            <li>Any Adora run needs full recordings.</li>
-            <li>Any hard rounds need to be recorded.</li>
-        </ol>
+        <h2>Tower Limitations</h2>
+        <p>Towers that have successfully soloed CHIMPS mode in the past or currently are <strong>not</strong> allowed. As of now, this includes <strong>Sauda</strong> and <strong>Geraldo</strong>.</p>
+        <h2>Challenge Settings</h2>
+        <p>
+            The Challenge Editor should be set to the hard difficulty and CHIMPS mode.
+            Starting Cash, Starting Lives, Max Lives, Start Round, and End Round must be set to their default values.
+            The end round must strictly be set to Round 100.
+            The "Least Cash" condition should be set to "No Score Set" (or 99999999 through clicking the auto button twice which does the same thing without needing a reset); this is mainly to prevent exploiting of cash bugs. You can reset the score after completing a run by clicking the auto button twice.
+            All sliders must be set to 100%.
+            All check boxes at the bottom must remain unchecked.
+        </p>
+        <h2>Tower and Hero Restrictions</h2>
+        <p>
+            Exclude all unused towers from your selection.
+            The towers' path and crosspath need to be restricted.
+            When using heroes, you must not select the "selected hero" option.
+        </p>
+        <h2>Submission Requirements</h2>
+        <p>
+            Victory Screen screenshots alone are insufficient for inclusion in The Index. You must also provide a screenshot of the final tower setup.
+            You must provide a challenge code that reflects the limitations and specifications outlined in the rules of this challenge.
+        </p>
+        <h2>Hard Round Recordings</h2>
+        <p>
+            For the successful completion of a submission, recordings of gameplay during hard rounds are mandatory. If a helper from the community requests recordings of a particular round, you will have 48 hours to provide the requested footage. Failure to do so within this timeframe may result in a temporary suspension or ban from future submissions.
+            Additionally, any runs featuring Adora must include full recordings of gameplay.
+        </p>
+        <h2>Fair Play and Game Integrity</h2>
+        <p>
+            No game modification, hacking, or unauthorized software use allowed. This includes the use of speedhacks and any other alterations that deviate from the game's original mechanics and settings. Violation leads to disqualification and potential exclusion from future challenges.
+        </p>
     </>;
 }
