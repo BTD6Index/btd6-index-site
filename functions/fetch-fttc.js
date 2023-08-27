64109faea1c734aba198980bee39ce88133674fa
Landmaster <python0429@gmail.com>
2023-08-27 13:29:22 -0400

    add towercount filter to fttc api

--- functions/fetch-fttc.js
+++ functions/fetch-fttc.js
@@ -5,11 +5,13 @@ export async function onRequest(context) {
         context,
         primaryFieldKeys: ['map', 'towerset'],
         personKeys: ['person'],
-        extraKeys: ['towerincludes'],
+        extraKeys: ['towerincludes', 'towercount'],
         challenge: 'fttc',
         customFieldQuery: ({field, idx, paramPos}) => {
             if (field === 'towerincludes') {
                 return `"fttc_completions_fts" = (SELECT 'towerset:"' || group_concat(REPLACE(towers.value, '"', '""'), ' ') || '"' FROM json_each(json_extract(?${paramPos}, '$[${idx}]')) AS towers)`;
+            } else if (field === 'towercount') {
+                return `json_array_length(towerset) = CAST(json_extract(?${paramPos}, '$[${idx}]') AS INTEGER)`;
             }
             return null;
         }
