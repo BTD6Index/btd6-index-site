e7b51527cc0440f9469c12548c1462f3ed5af99f
Landmaster <python0429@gmail.com>
2023-09-01 14:50:26 -0400

    refactoring

--- functions/member/delete-lcc-submit.js
+++ functions/member/delete-lcc-submit.js
@@ -1,37 +1,5 @@
+import { handleDeleteSubmitLCCLike } from "./handleDeleteSubmit";
+
 export async function onRequest(context) {
-    const db = context.env.BTD6_INDEX_DB;
-    const media = context.env.BTD6_INDEX_MEDIA;
-    const jwtResult = context.data.jwtResult;
-    const isHelper = jwtResult.payload.permissions.includes('write:admin');
-
-    const respondError = (error) => {
-        return Response.json({error}, {status: 400});
-    };
-
-    if (context.request.method !== "POST") {
-        return respondError(`Request method should be POST, got ${context.request.method}`);
-    }
-
-    let formData = await context.request.formData();
-    if (!formData.has('entries')) {
-        return respondError(`Need lcc entries to delete passed in`);
-    }
-
-    let filekeys = await db.prepare(`DELETE FROM lcc_completions AS cmp WHERE EXISTS `
-    + `(SELECT 1 FROM json_each(?1) `
-    + `WHERE cmp.filekey = json_extract(value, '$[0]') `
-    + `AND ${isHelper ? '?2 = ?2' : 'cmp.pending = ?2'}) RETURNING filekey`)
-    .bind(formData.get('entries'), jwtResult.payload.sub /* user id */)
-    .all();
-
-    for (let row of filekeys.results) {
-        context.waitUntil(
-            media.list({prefix: row.filekey})
-            .then(async (listRes) => {
-                await media.delete(listRes.objects.map(obj => obj.key));
-            })
-        );
-    }
-
-    return Response.json({});
+    return handleDeleteSubmitLCCLike({context, challenge: 'lcc'});
 }
