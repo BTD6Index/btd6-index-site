8b78f6edfb76d34326df01065d4156515d5e5bf9
Landmaster <python0429@gmail.com>
2023-09-04 10:09:06 -0400

    modify popular challenges

--- src/routes/HomePage.jsx
+++ src/routes/HomePage.jsx
@@ -3,8 +3,8 @@ function HomePage() {
         <h1>High-Level Bloons TD 6 Challenges</h1>
         <p>We are a community of Bloons TD 6 players tracking various community-created challenges.</p>
         <h2>Popular Challenges</h2>
-        <p><a href='/2mp'>2 Million Pops CHIMPS</a> - Complete CHIMPS with 2 million pops on a tower (excluding regrow pops)</p>
-        <p><a href='/2tc'>2 Towers CHIMPS</a> - Complete CHIMPS with 2 towers</p>
+        <p><a href='/2mp'>2 Million Pops CHIMPS</a> - Complete a BTD6 CHIMPS game with 2 million pops on a tower (excluding regrow pops)</p>
+        <p><a href='/2tc'>2 Towers CHIMPS</a> - Complete a BTD6 CHIMPS game with 2 towers</p>
         <h2>Latest Video</h2>
         <iframe id="latestvid" src="https://www.youtube-nocookie.com/embed?listType=playlist&list=UULFFbnVzrtC3yShaAqwbAsyPw" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
         <h2>Join Us</h2>
