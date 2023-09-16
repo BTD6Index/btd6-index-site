f436251ce2245cbfc017dfbfc67da3f18febb0e4
Landmaster <python0429@gmail.com>
2023-09-15 22:30:08 -0400

    attempt to login with popup if access token retrieval fails

--- src/util/useAccessToken.jsx
+++ src/util/useAccessToken.jsx
@@ -2,5 +2,16 @@ import { useAuth0 } from "@auth0/auth0-react";
 
 export default function useAccessToken() {
     const {getAccessTokenSilently, getAccessTokenWithPopup} = useAuth0();
-    return window.location.hostname.endsWith('btd6index.win') ? getAccessTokenSilently : getAccessTokenWithPopup;
+    return window.location.hostname.endsWith('btd6index.win')
+    ? async (tokenParams) => {
+        try {
+            return await getAccessTokenSilently(tokenParams);
+        } catch (e) {
+            if (e.error === 'login_required' || e.error === 'consent_required') {
+                return await getAccessTokenWithPopup();
+            }
+            throw e;
+        }
+    }
+    : getAccessTokenWithPopup;
 }
