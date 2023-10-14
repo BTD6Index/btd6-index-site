d302b27937098e4bc54f224d8e1e88a484caa731
Landmaster <python0429@gmail.com>
2023-10-14 18:25:49 -0400

    do not accidentally filter moab dom

--- functions/sanitizeDiscord.js
+++ functions/sanitizeDiscord.js
@@ -1,6 +1,7 @@
 // adapted from https://github.com/Swiiz/discord-escape
 
 export default function sanitizeDiscord(str) {
+    return str; // TODO remove this if we ever enable submissions from non-helpers again
     return str
     .replace(/(_|\*|~|`|\||\\|<|>|!|(?<=^|\n|\r)#+\s|(?<=(?:^|\n|\r)\s*)-\s)/g, "\\$1")
     .replace(/((?<=(?:^|\n|\r)\s*)\d+)\.(\s)/g, "$1\\.$2")
