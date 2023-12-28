1134462c5e25be4ebb4b9a1f943f99ea84e78e15
Landmaster <python0429@gmail.com>
2023-12-27 20:14:10 -0500

    should fix ltc sorting

--- functions/fetch-ltc.js
+++ functions/fetch-ltc.js
@@ -5,6 +5,14 @@ export async function onRequest(context) {
         context,
         databaseTable: "ltc_completions_fts",
         fields: ['map', 'towerset', 'link', 'completiontype', 'pending', 'upgradeset', 'version', 'date', 'filekey'],
-        personFields: ['person']
+        personFields: ['person'],
+        sortByIndex: {
+            'map': 'map',
+            'map DESC': 'map DESC',
+            'towerset': 'json_array_length(towerset), towerset',
+            'towerset DESC': 'json_array_length(towerset) DESC, towerset DESC',
+            'completiontype': 'completion_type',
+            'completiontype DESC': 'completion_type DESC'
+        },
     });
 }
