e6b44061fbf85fc3fe91317da86f130a49e8d34f
alexmi3 <n1xbl4d3@gmail.com>
2024-06-08 09:54:06 -0400

    sort odysseys by date, thead zindex

--- functions/list-odysseys.js
+++ functions/list-odysseys.js
@@ -2,7 +2,7 @@ export async function onRequest(context) {
     const db = context.env.BTD6_INDEX_DB;
     let searchParams = new URL(context.request.url).searchParams;
     let res = await db
-    .prepare('SELECT odysseyName FROM odyssey_information')
+    .prepare('SELECT odysseyName FROM odyssey_information ORDER BY startDate')
     .all();
     return Response.json({results: res.results.map(v => v.odysseyName)});
 }
