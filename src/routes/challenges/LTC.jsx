890a1bf8404c1ab624fcae9ae0773c83571a2f0f
alexmi3 <n1xbl4d3@gmail.com>
2025-02-14 11:05:41 -0500

    ltc

--- src/routes/challenges/LTC.jsx
+++ src/routes/challenges/LTC.jsx
@@ -1,5 +1,6 @@
 import ChallengePage from "./ChallengePage";
-import { useCallback } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
+import { defaultRules, addRule } from "../../util/rules";
 
 export default function LTC() {
     const fieldDisplayFunc = useCallback(({fieldName, fieldValue}) => {
@@ -19,6 +20,38 @@ export default function LTC() {
         }
         return fieldValue ?? 'N/A';
     }, []);
+
+    const effectRan = useRef(false);
+    const [allRules, setAllRules] = useState(defaultRules);
+    
+    useEffect(() => {
+        if(effectRan.current === true){
+            setAllRules(a => [...addRule(a, {
+                id: 1, 
+                name: 'Tower Restrictions', 
+                rule: <div>
+                        <h2>Tower and Hero Restrictions</h2>
+                        <p>Exclude all unused towers from your selection. The towers' path and crosspath need to be restricted.</p>
+                        <p>When using heroes, you must not select the "selected hero" option.</p>
+                    </div>,
+            })]);
+
+            setAllRules(a => [...addRule(a, {
+                id: 2, 
+                name: 'Alternate Completions', 
+                rule: <div>
+                        <h2>Alternate Combos</h2>
+                        <p>For each map the only combos counted are: </p>
+                        <ul>
+                            <li><strong>The OG</strong>: The least amount of towers completed on the map, tiebroken by the earliest of those combos.</li>
+                            <li><strong>The Cheapest</strong>: The cheapest combo using only the number of towers from the OG.</li>
+                        </ul>
+                    </div>,
+            })]);
+        }
+        return () => {effectRan.current = true}
+    }, [])
+
     return <ChallengePage
         challenge="ltc"
         header="Least Towers CHIMPS"
