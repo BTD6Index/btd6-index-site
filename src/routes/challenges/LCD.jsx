0cd880095024df3df3b554c1ec6d472dd4d74e50
alexmi3 <n1xbl4d3@gmail.com>
2025-02-14 11:04:32 -0500

    lcd

--- src/routes/challenges/LCD.jsx
+++ src/routes/challenges/LCD.jsx
@@ -1,6 +1,43 @@
 import ChallengePage from "./ChallengePage";
+import { defaultRules, addRule, deleteRule, editRule } from "../../util/rules";
+import { useEffect, useRef, useState } from "react";
 
 export default function LCD() {
+    const effectRan = useRef(false);
+    const [allRules, setAllRules] = useState(defaultRules);
+    
+    useEffect(() => {
+        if(effectRan.current === true){
+            setAllRules(a => [...addRule(a, {
+                id: 1, 
+                name: 'Version Requirements', 
+                rule: <div>
+                        <h2>Version-Related Requirements</h2>
+                        <p>Least Cost Deflation completions are independent of version and can be submitted on any version.</p>
+                    </div>,
+            })]);
+
+            setAllRules(a => [...editRule(a, 'Challenge Settings', {
+                id: 101,
+                name: 'Challenge Settings',
+                rule: <div>
+                        <h2>Challenge Settings</h2>
+                        <ul>
+                            <li>The Challenge Editor should be set to the easy difficulty and Deflation mode. </li>
+                            <li>Starting Cash, Starting Lives, Max Lives, Start Round, and End Round must be set to their default values.</li> 
+                            <li>The end round must strictly be set to Round 60.</li>
+                            <li>The "Least Cash" condition should initially be set to "No Score Set". <strong>Unlike most other Index challenges, setting the condition to 99999999 cash is not allowed, as the challenge code needs to incorporate the cash spent.</strong></li>
+                            <li>All sliders must be set to 100%.</li>
+                            <li>Monkey Knowledge is allowed, but no powers, continues, or <strong>selling</strong> are allowed.</li>
+                        </ul>
+                    </div>,
+            })])
+            
+            setAllRules(a => [...deleteRule(a, 'Adora')])
+        }
+        return () => {effectRan.current = true}
+    }, [])
+
     return <ChallengePage
         challenge="lcd"
         header="Least Cost Deflation"
