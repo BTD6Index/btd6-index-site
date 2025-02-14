83fe82642bb5f129a38cb48b6024e4777a276b53
alexmi3 <n1xbl4d3@gmail.com>
2025-02-14 11:12:02 -0500

    rule changes

--- src/routes/challenges/TwoTC.jsx
+++ src/routes/challenges/TwoTC.jsx
@@ -1,6 +1,34 @@
-import ChallengePage from "./ChallengePage"
+import ChallengePage from "./ChallengePage";
+import { defaultRules, addRule } from "../../util/rules";
+import { useEffect, useState, useRef } from "react";
 
 export default function TwoTC() {
+    const effectRan = useRef(false);
+    const [allRules, setAllRules] = useState(defaultRules);
+    
+    useEffect(() => {
+        if(effectRan.current === true){
+            setAllRules(a => [...addRule(a, {
+                id: 1, 
+                name: 'Tower Limitations', 
+                rule: <div>
+                        <h2>Tower Limitations</h2>
+                        <p>Towers that have successfully soloed CHIMPS mode in the past or currently are <strong>not</strong> allowed. As of now, this includes <strong>Sauda</strong>, <strong>Geraldo</strong>, and <strong>Corvus</strong>.</p>
+                    </div>,
+            })]);
+
+            setAllRules(a => [...addRule(a, {
+                id: 1, 
+                name: 'Tower Restrictions', 
+                rule: <div>
+                    <h2>Tower and Hero Restrictions</h2>
+                    <p>Exclude all unused towers from your selection. The towers' path and crosspath need to be restricted. When using heroes, you must not select the "selected hero" option.</p>
+                </div>,
+            })]);
+        }
+        return () => {effectRan.current = true}
+    }, [])
+
     return <ChallengePage
     challenge="2tc"
     header="2 Towers CHIMPS"
@@ -16,5 +44,6 @@ export default function TwoTC() {
         return fieldValue || 'N/A';
     }}
     hasVersion
+    rules={allRules}
     />
 };
