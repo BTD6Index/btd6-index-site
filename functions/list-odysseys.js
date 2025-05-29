30edfc0ad903af358b5b52f9d205fe98bf4ea06b
alexmi3 <n1xbl4d3@gmail.com>
2025-05-29 09:58:28 -0400

    remove odyssey/lto

--- functions/list-odysseys.js
+++
@@ -1,8 +0,0 @@
-export async function onRequest(context) {
-    const db = context.env.BTD6_INDEX_DB;
-    let searchParams = new URL(context.request.url).searchParams;
-    let res = await db
-    .prepare('SELECT odysseyName FROM odyssey_information ORDER BY startDate')
-    .all();
-    return Response.json({results: res.results.map(v => v.odysseyName)});
-}
