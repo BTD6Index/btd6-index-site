404f49b948a4d7df85380c3884d085253482692d
Landmaster <python0429@gmail.com>
2025-10-19 15:45:51 -0400

    temp remove rate limiting

--- functions/download-2tc-csv.js
+++ functions/download-2tc-csv.js
@@ -1,10 +1,10 @@
 import { handleDownloadCSV } from "./handleFetch";
 
 export async function onRequest(context) {
-    const { success } = await context.env.DATA_DUMP_DOWNLOAD_RL.limit({ key: "download-2tc-csv" });
-    if (!success) {
-        return new Response({"error": "rate limit exceeded"}, {status: 429})
-    }
+    // const { success } = await context.env.DATA_DUMP_DOWNLOAD_RL.limit({ key: "download-2tc-csv" });
+    // if (!success) {
+    //     return new Response({"error": "rate limit exceeded"}, {status: 429})
+    // }
     const csv = await handleDownloadCSV({
         context,
         primaryFieldKeys: ['tower1', 'tower2'],
