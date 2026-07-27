92d5bbb4ecd910549de3152b99ba699a79e5705a
Chocbox <chocbox3@gmail.com>
2026-07-26 19:31:02 -0700

    fix html

--- src/routes/challenges/manip/ManipTwoTC.jsx
+++ src/routes/challenges/manip/ManipTwoTC.jsx
@@ -81,8 +81,17 @@ function ManipTwoTC({editParams = null, setEditParams = null}) {
                 <input name="person" type="text" placeholder="Person" style={{width: '20ch'}} defaultValue={existingInfo?.[0]?.person} required />
             </span>
             <br />
+            <span className="formLine">
+                <label htmlFor="challenge_code">Challenge Editor Code (only the raw code)</label>
                 <input
-                    id="challenge_code" name="challenge_code" type="text" placeholder="Challenge Editor Code" style={{width: '20ch'}} defaultValue={noteInfo?.challengeCode ?? existingInfo?.[0]?.challengeCode} />
+                    id="challenge_code"
+                    name="challenge_code"
+                    type="text"
+                    placeholder="Challenge Editor Code"
+                    style={{width: '20ch'}}
+                    defaultValue={noteInfo?.challengeCode ?? existingInfo?.[0]?.challengeCode}
+                />
+            </span>
             <br />
             <FormLinkImageEntry existingInfo={existingInfo} />
             <br />
