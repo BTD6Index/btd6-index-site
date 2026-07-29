ce9e055baa0904f25a6f90b00ff48ce27413f55b
Chocbox <chocbox3@gmail.com>
2026-07-28 21:34:32 -0700

    semicolon fix

--- src/routes/challenges/manip/ManipTwoTC.jsx
+++ src/routes/challenges/manip/ManipTwoTC.jsx
@@ -37,8 +37,8 @@ function ManipTwoTC({editParams = null, setEditParams = null}) {
         setSubmissionInProgress
     });
 
-    const filteredTowerOptions = [...towerToOptions.values()]
-    const filteredStartingTowerOptions = [...startingTowerToOptions.values()]
+    const filteredTowerOptions = [...towerToOptions.values()];
+    const filteredStartingTowerOptions = [...startingTowerToOptions.values()];
 
 
     const [map, setMap] = useState(null);
