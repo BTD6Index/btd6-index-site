e51679474b9f3daedc35429ae983ffc75d4e04ff
Landmaster <python0429@gmail.com>
2026-09-03 19:55:34 -0400

    uninstall unused plugin

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import {yamlPlugin} from "vite-yaml-plugin";
-import { cloudflare } from "@cloudflare/vite-plugin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), yamlPlugin(), cloudflare()],
+  plugins: [react(), yamlPlugin()],
 });
