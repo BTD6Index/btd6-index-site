331eec2ad4efa7cab2571193000946f79f8af33d
Landmaster <python0429@gmail.com>
2026-09-15 22:00:43 -0400

    fix 2mp person stats

--- functions/fetch-2mp-person-counts.js
+++ functions/fetch-2mp-person-counts.js
@@ -10,7 +10,7 @@ export async function onRequest(context) {
     (SELECT count(DISTINCT map) FROM twomp_completions WHERE person = person0) AS uniquecount,
     person0 AS person,
     (SELECT map FROM twomp_completions WHERE person = person0 GROUP BY map ORDER BY count(*) DESC LIMIT 1) AS favoritemap,
-    (SELECT difficulty FROM twomp_completions INNER JOIN map_information ON twomp_completions.map = map_information.map WHERE person = person0 GROUP BY twomp_completions.difficulty ORDER BY count(*) DESC LIMIT 1) AS favoritedifficulty
+    (SELECT difficulty FROM twomp_completions INNER JOIN map_information ON twomp_completions.map = map_information.map WHERE person = person0 GROUP BY difficulty ORDER BY count(*) DESC LIMIT 1) AS favoritedifficulty
     FROM (SELECT DISTINCT person AS person0 FROM twomp_completions)
     ORDER BY count DESC LIMIT $2 OFFSET $1`).bind(offset, limit).all();
     return Response.json({personData: res.results});
